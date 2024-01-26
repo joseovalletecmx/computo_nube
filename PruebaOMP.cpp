72d5ed32147b8d04bff825f26657a11e79bbf854
joseovalle_a01150742 <113394492+joseovalletecmx@users.noreply.github.com>
2024-01-26 15:11:39 -0600

    codigo_actividad_programacion_solucion_paralela

---
+++ PruebaOMP.cpp
@@ -0,0 +1,54 @@
+// PruebaOMP.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+//#include "pch.h"
+
+#include <iostream>
+#include <omp.h>
+
+#define N 1000
+#define chunk 100
+#define mostrar 10
+
+
+// Realiza varias ejecuciones cambiando algunos datos como
+// tamaño de los arreglos N, chunk y la cantidad de datos a mostrar 
+
+
+
+void imprimeArreglo(float *d);
+
+int main()
+{
+	std::cout << "Sumando Arreglos en Paralelo!\n";
+	float a[N], b[N], c[N];
+	int i;
+
+	for (i = 0; i < N; i++)
+	{
+		a[i] = i * 30;
+		b[i] = (i + 3) * 3.7;
+	}
+	int pedazos = chunk;
+
+	#pragma omp parallel for \
+	shared(a, b, c, pedazos) private(i) \
+	schedule(static, pedazos)
+
+	for (i = 0; i < N; i++)
+		c[i] = a[i] + b[i];
+	
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
+	imprimeArreglo(a);
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
+	imprimeArreglo(b);
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo c: " << std::endl;
+	imprimeArreglo(c);
+}
+
+void imprimeArreglo(float* d)
+{
+	for (int x = 0; x < mostrar; x++)
+		std::cout << d[x] << " - ";
+	std::cout << std::endl;
+}
